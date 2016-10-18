2330f98cac8963c340945a97b7c25c6f0474b385
Royston <rlqhenson@hotmail.com>
2016-10-18 21:47:23 +0100

    add release_bike and dock_bike tests and methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
+require_relative 'bike'
+
 class DockingStation
 
-  def release_bike
+attr_reader :bikerack
 
+  def initialize
+    @bikerack = []
   end
 
+  def release_bike
+    @bikerack.pop
+  end
+
+  def dock_bike(bike)
+    @bikerack << bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,26 @@
 require "docking_station"
 
 describe DockingStation do
-subject {described_class.new}
-let(:bike) { double("bike", :working => true) }
+  subject {described_class.new}
+  let(:bike) { double("bike", :working => true) }
 
-  it "responds to #release_bike" do
-    expect(subject).to respond_to(:release_bike)
+  describe "#release_bike" do
+    it "responds to #release_bike" do
+      expect(subject).to respond_to(:release_bike)
+    end
+    it "releases a working bike" do
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
   end
-  it "releases a working bike" do
-    expect(bike.working).to be(true)
+  describe "#dock_bike" do
+    it "responds to #dock_bike" do
+      expect(subject).to respond_to(:dock_bike)
+    end
+    it "stores bike in docking station" do
+      subject.dock_bike(bike)
+      expect(subject.bikerack).to include(bike)
+    end
   end
+
 end
