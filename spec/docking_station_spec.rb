2db4dd486691b15239690c288a8f6192d9641921
Royston <rlqhenson@hotmail.com>
2016-10-18 21:19:16 +0100

    add docking station releases working bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,12 @@ require "docking_station"
 
 describe DockingStation do
 subject {described_class.new}
+let(:bike) { double("bike", :working => true) }
 
-  it "reponds to #release_bike" do
+  it "responds to #release_bike" do
     expect(subject).to respond_to(:release_bike)
+  end
+  it "releases a working bike" do
+    expect(bike.working).to be(true)
   end
 end
